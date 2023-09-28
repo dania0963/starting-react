5a3187e27a7425a319c9e8017ea33ba3092e7534
Dania <daniamdqt@gmail.com>
2023-09-28 12:54:54 +0300

    test jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{margin:"auto",paddingTop: 30}}>
+      <h1 className="title">pokemon test</h1>
+      <table width="70%">
+        <thead>
+          <tr>
+          <th>first</th>
+          <th>second</th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }
